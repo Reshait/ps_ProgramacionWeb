5127804912b772b543e5ad1bf55b6195a8424732
Reshait <reshait@outlook.com>
2016-08-10 23:21:12 +0200

    home.html -- OK

--- projectBlogTeo/appBlogTeo/models.py
+++ projectBlogTeo/appBlogTeo/models.py
@@ -1,5 +1,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Entrada(models.Model):
+	titulo = models.CharField(max_length = 100)
+	cuerpo = models.TextField()
+	fecha = models.DateTimeField(default=datetime.now, blank=True)
+
+	def __unicode__ (self):
+		return self.titulo
--- projectBlogTeo/appBlogTeo/views.py
+++ projectBlogTeo/appBlogTeo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Entrada
 
 # Create your views here.
+def home(request):
+	listadoEntradas = Entrada.objects.order_by('-fecha')[:5]
+	context = {'listadoEntradas': listadoEntradas}
+	return render(request, 'home.html', context)
+	#return HttpResponse(response % Entrada_id)
