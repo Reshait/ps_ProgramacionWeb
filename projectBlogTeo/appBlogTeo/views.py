a2f63d1f1dfa5919c9fa7facbbb443037a3e7165
Reshait <reshait@outlook.com>
2016-08-10 23:42:26 +0200

    Añadido base.html

--- projectBlogTeo/appBlogTeo/views.py
+++ projectBlogTeo/appBlogTeo/views.py
@@ -3,7 +3,9 @@ from .models import Entrada
 
 # Create your views here.
 def home(request):
-	listadoEntradas = Entrada.objects.order_by('-fecha')[:5]
+	listadoEntradas = Entrada.objects.order_by('-fecha')[:4]
 	context = {'listadoEntradas': listadoEntradas}
 	return render(request, 'home.html', context)
-	#return HttpResponse(response % Entrada_id)
+
+#def entradaCompleta(request):
+
