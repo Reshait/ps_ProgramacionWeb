558c4ccc412d1c934594535785b83b382bb5b38c
Reshait <reshait@outlook.com>
2016-08-18 02:12:12 +0200

    OK Plantilla CSSBootstrap

--- projectBlogTeo/projectBlogTeo/settings.py
+++ projectBlogTeo/projectBlogTeo/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Para app gestion de usuaios
 LOGIN_URL = '/cuentasUsuario/login/'
