5a8ed041703a533c6b398a02aff0b6dc64f5d38f
Reshait <reshait@outlook.com>
2016-08-21 19:50:35 +0200

    OK Acerca

--- projectBlogTeo/appBlogTeo/urls.py
+++ projectBlogTeo/appBlogTeo/urls.py
@@ -6,6 +6,6 @@ app_name = 'appBlogTeo'
 urlpatterns = [
 	url(r'^$', views.ListadoHome, name='home'),
 	url(r'^entrada/(?P<pk>.*)$', views.VistaEntradaCompleta.as_view(), name='VistaEntradaCompleta'),
-    url(r'^acerca/$', views.VistaAcerca.as_view(), name='VistaAcerca'),
+    url(r'^acerca/$', views.VistaAcerca.as_view(), name='acerca'),
 
 ]
